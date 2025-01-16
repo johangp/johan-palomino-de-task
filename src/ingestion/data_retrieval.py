ac05020593e7d03863a460782dfb7d56954d9b75
Johan Palomino <johanpalomino59@gmail.com>
2025-01-16 19:32:29 +0100

    Manage api calls limits

--- src/ingestion/data_retrieval.py
+++ src/ingestion/data_retrieval.py
@@ -1,12 +1,16 @@
+import time
 from datetime import datetime, timedelta
 from typing import Any
 
 import requests
+from tenacity import retry, stop_after_attempt, wait_fixed
 
 from .exceptions import InvalidApiKey
 
 LISTS_OVERVIEW_ENDPOINT = "https://api.nytimes.com/svc/books/v3/lists/overview.json"
 API_KEY = "H4K4FyQYZKhhlTwLitBhDG5zDjtC2Cib"  # Harcoded for the sake of simplicity
+DATE_FORMAT = "%Y-%m-%d"
+SECONDS_BETWEEN_CALLS = 12  # Limit to don't hit the rate limit call per minute. More info  in the endpoint's FAQ
 
 
 class DataRetrieval:
@@ -24,6 +28,7 @@ class DataRetrieval:
                 self.end_date if current_date > self.end_date else current_date
             )
 
+    @retry(stop=stop_after_attempt(5), wait=wait_fixed(SECONDS_BETWEEN_CALLS))
     def _request_data(self, published_date: str) -> Any:
         params = {"api-key": API_KEY, "publisd_date": published_date}
         response = requests.get(LISTS_OVERVIEW_ENDPOINT, params=params)
@@ -40,4 +45,10 @@ class DataRetrieval:
         The New York Time API. It downloads the data from the provided date interval
         and stores it into a PostgreSQL database.
         """
-        pass
+        for week in self._week_generator():
+            last_request_time = time.time()
+            content = self._request_data(week.strftime(DATE_FORMAT))
+            time_diff = time.time() - last_request_time
+
+            if time_diff <= SECONDS_BETWEEN_CALLS:
+                time.sleep(SECONDS_BETWEEN_CALLS - time_diff)
