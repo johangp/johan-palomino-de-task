0bc306a1927f6b92de0a598208ec6d76b43f0854
Johan Palomino <johanpalomino59@gmail.com>
2025-01-17 04:33:35 +0100

    Deduplicate staging models

--- new_york_times/models/staging/stg_raw_books.sql
+++ new_york_times/models/staging/stg_raw_books.sql
@@ -16,10 +16,15 @@ with
             title,
             created_date,
             updated_date,
-            created_at
+            created_at,
+            row_number() over (
+                partition by list_id, primary_isbn13
+                order by updated_date desc, created_at desc
+            ) as row_num
         from {{ source("raw_data", "raw_books") }}
     )
 
 select *
 from raw_books
+where row_num = 1
 
--- new_york_times/models/staging/stg_raw_lists.sql
+++ new_york_times/models/staging/stg_raw_lists.sql
@@ -7,10 +7,12 @@ with
             display_name,
             updated,
             list_image,
-            created_at
+            created_at,
+            row_number() over (partition by list_id order by created_at desc) as row_num
         from {{ source("raw_data", "raw_lists") }}
     )
 
 select *
 from raw_lists
+where row_num = 1
 
