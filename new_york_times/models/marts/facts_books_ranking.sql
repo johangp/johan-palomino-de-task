e5bfb99461c717fa45c402b2f59434d3e45f59c7
Johan Palomino <johanpalomino59@gmail.com>
2025-01-17 04:36:32 +0100

    Add facts books ranking model

---
+++ new_york_times/models/marts/facts_books_ranking.sql
@@ -0,0 +1,18 @@
+with
+    rankings as (
+        select
+            row_number() over () as ranking_id,
+            {{ dbt_utils.generate_surrogate_key(["b.title", "b.author"]) }} as book_id,
+            l.list_id,
+            r.results_id,
+            b.rank,
+            r.bestsellers_date,
+            r.published_date
+        from {{ ref("stg_raw_books") }} b
+        join {{ ref("stg_raw_lists") }} l on b.list_id = l.list_id
+        join {{ ref("stg_raw_results") }} r on b.results_id = r.results_id
+    )
+
+select *
+from rankings
+
